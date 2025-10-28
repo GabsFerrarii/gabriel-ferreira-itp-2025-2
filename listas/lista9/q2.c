d1406d02ce1e148f0a40a9e801202b0ebc377728
Gabriel Ferreira <cavalcante.gabriel@escolar.ifrn.edu.br>
2025-10-27 23:47:40 -0300

    fix: update q2

--- listas/lista9/q2.c
+++ listas/lista9/q2.c
@@ -5,35 +5,42 @@ int main() {
     scanf("%d", &m);
 
     int amizades[100][100];
-    int usuarioX;
-
+    
     for (int i = 0; i < m; i++) {
         for (int j = 0; j < m; j++) {
             scanf("%d", &amizades[i][j]);
         }
     }
 
+    int usuarioX;
     scanf("%d", &usuarioX);
 
-    for (int i = 0; i < m; i++) {
-        if (i == usuarioX) {
+    int primeiraSugestao = 1;
+
+    for (int j = 0; j < m; j++) {
+        if (j == usuarioX || amizades[usuarioX][j] == 1) {
             continue;
         }
 
-        if (amizades[usuarioX][i] == 0) {
-            int amigoEmComum = 0;
-            for (int j = 0; j < m; j++) {
-                if (amizades[usuarioX][j] == 1 && amizades[i][j] == 1) {
-                    amigoEmComum = 1;
-                    break;
-                }
+        int temAmigoEmComum = 0;
+        for (int k = 0; k < m; k++) {
+            if (amizades[usuarioX][k] == 1 && amizades[j][k] == 1) {
+                temAmigoEmComum = 1;
+                break;
             }
+        }
 
-            if (amigoEmComum) {
-                printf("%d\n", i);
+        if (temAmigoEmComum) {
+            if (primeiraSugestao == 0) {
+                printf(" ");
             }
+            printf("%d", j);
+            primeiraSugestao = 0;
         }
     }
 
+    printf("\n");
+
     return 0;
 }
+
